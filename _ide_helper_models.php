5c292c73a9544496664dcd9f218a4b9320257fe8
Sergey Indenbom <dzorogh@gmail.com>
2024-11-09 16:13:40 +0300

    chore: ide-helper models

---
+++ _ide_helper_models.php
@@ -0,0 +1,362 @@
+<?php
+
+// @formatter:off
+// phpcs:ignoreFile
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Event> $events
+ * @property-read int|null $events_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|City whereUpdatedAt($value)
+ */
+	class City extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $inn
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $description
+ * @property string|null $website
+ * @property string|null $phone
+ * @property string|null $email
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Event> $events
+ * @property-read int|null $events_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereInn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Company whereWebsite($value)
+ */
+	class Company extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int|null $series_id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $start_date
+ * @property \Illuminate\Support\Carbon|null $end_date
+ * @property \App\Enums\EventFormat $format
+ * @property int|null $city_id
+ * @property int $industry_id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $website
+ * @property bool $is_priority
+ * @property int $sort_order
+ * @property string|null $description
+ * @property string|null $phone
+ * @property string|null $email
+ * @property int|null $venue_id
+ * @property-read \App\Models\City|null $city
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Company> $companies
+ * @property-read int|null $companies_count
+ * @property-read \App\Models\Industry $industry
+ * @property-read \Spatie\MediaLibrary\MediaCollections\Models\Collections\MediaCollection<int, \Spatie\MediaLibrary\MediaCollections\Models\Media> $media
+ * @property-read int|null $media_count
+ * @property-read \App\Models\Series|null $series
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Speaker> $speakers
+ * @property-read int|null $speakers_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tag> $tags
+ * @property-read int|null $tags_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Tariff> $tariffs
+ * @property-read int|null $tariffs_count
+ * @property-read \App\Models\Venue|null $venue
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event active()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereCityId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereEndDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereFormat($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereIndustryId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereIsPriority($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereSeriesId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereStartDate($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereVenueId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Event whereWebsite($value)
+ */
+	class Event extends \Eloquent implements \Spatie\MediaLibrary\HasMedia {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int $sort_order
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Event> $events
+ * @property-read int|null $events_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Industry whereUpdatedAt($value)
+ */
+	class Industry extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $slug
+ * @property array $filters
+ * @property bool $is_active
+ * @property int $sort_order
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereFilters($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Preset whereUpdatedAt($value)
+ */
+	class Preset extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Event> $events
+ * @property-read int|null $events_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Series newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Series newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Series query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Series whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Series whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Series whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Series whereUpdatedAt($value)
+ */
+	class Series extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string|null $middle_name
+ * @property string $last_name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Event> $events
+ * @property-read int|null $events_count
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Topic> $topics
+ * @property-read int|null $topics_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker whereMiddleName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Speaker whereUpdatedAt($value)
+ */
+	class Speaker extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Event> $events
+ * @property-read int|null $events_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tag whereUpdatedAt($value)
+ */
+	class Tag extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $event_id
+ * @property numeric $price
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $title
+ * @property bool $is_active
+ * @property int $sort_order
+ * @property-read \App\Models\Event $event
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereEventId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereIsActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff wherePrice($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereSortOrder($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Tariff whereUpdatedAt($value)
+ */
+	class Tariff extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property int $speaker_id
+ * @property string $title
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Speaker|null $speakers
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic whereSpeakerId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Topic whereUpdatedAt($value)
+ */
+	class Topic extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|User whereUpdatedAt($value)
+ */
+	class User extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * 
+ *
+ * @property int $id
+ * @property string $title
+ * @property string|null $description
+ * @property string|null $address
+ * @property string|null $website
+ * @property string|null $phone
+ * @property string|null $email
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Event> $events
+ * @property-read int|null $events_count
+ * @method static \Database\Factories\VenueFactory factory($count = null, $state = [])
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Venue whereWebsite($value)
+ */
+	class Venue extends \Eloquent {}
+}
+
