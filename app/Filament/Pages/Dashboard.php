2a7899ea0c7d2a03c214bbe3be0a82898fa56a21
Sergey Indenbom <dzorogh@gmail.com>
2024-11-09 14:38:28 +0300

    refactor: BaseDashboard imports

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -2,21 +2,16 @@
 
 namespace App\Filament\Pages;
 
-use App\Models\Event;
-use App\Models\Company;
-use App\Models\Speaker;
 use Filament\Pages\Dashboard as BaseDashboard;
-use Filament\Widgets\StatsOverviewWidget\Stat;
-use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 
 class Dashboard extends BaseDashboard
 {
     public function getWidgets(): array
     {
         return [
-            DashboardStatsOverview::class,
-            UpcomingEventsWidget::class,
-            RecentCompaniesWidget::class,
+            \App\Filament\Widgets\DashboardStatsOverview::class,
+            \App\Filament\Widgets\UpcomingEventsWidget::class,
+            \App\Filament\Widgets\RecentCompaniesWidget::class,
         ];
     }
-} 
+}
