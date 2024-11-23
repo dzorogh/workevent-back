2ceea6e6e1284f0b78306355ea6b59b81226a243
Sergey Indenbom <dzorogh@gmail.com>
2024-11-23 18:58:32 +0300

    feat: industries cache

--- app/Http/Controllers/Api/V1/IndustryController.php
+++ app/Http/Controllers/Api/V1/IndustryController.php
@@ -4,20 +4,24 @@ namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Controllers\Controller;
 use App\Http\Resources\IndustryResource;
+use App\Models\City;
 use App\Models\Industry;
 use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Support\Facades\Cache;
 
 class IndustryController extends Controller
 {
     public function index(): AnonymousResourceCollection
     {
-        $industries = Industry::query()
-            ->whereHas('events', function ($query) {
-                $query->active();  // Assuming you have an active scope
-            })
-            ->withCount('events')
-            ->orderBy('sort_order')
-            ->get();
+        $industries = Cache::remember('active_industries', 3600, function () {
+            return Industry::query()
+                ->whereHas('events', function ($query) {
+                    $query->active();  // Assuming you have an active scope
+                })
+                ->withCount('events')
+                ->orderBy('sort_order')
+                ->get();
+        });
 
         return IndustryResource::collection($industries);
     }
